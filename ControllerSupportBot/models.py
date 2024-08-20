882180cb216e8387c38450f500636d0813ab3f92
KodyTomochko <kody.tomochko@swatmaps.com>
2024-08-20 16:37:04 -0600

    updated models and views

--- ControllerSupportBot/models.py
+++ ControllerSupportBot/models.py
@@ -10,4 +10,26 @@ class MyModelAdmin(admin.ModelAdmin):
         models.TextField: {'widget': TinyMCE()}
     }
 
+class ControllerManufacturer(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Controller(models.Model):
+    manufacturer = models.ForeignKey(ControllerManufacturer, on_delete=models.CASCADE, related_name='controllers')
+    model = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.manufacturer.name} - {self.model}"
+
+class SupportWorkflow(models.Model):
+    controller = models.ForeignKey(Controller, on_delete=models.CASCADE, related_name='support_workflows')
+    question = models.CharField(max_length=255)
+    answer = RichTextField()  # Changed to RichTextField
+
+    def __str__(self):
+        return f"{self.controller.model} - {self.question}"
+
 admin.site.register(MyModel, MyModelAdmin)
--- ControllerSupportBot/views.py
+++ ControllerSupportBot/views.py
@@ -1,3 +1,71 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import ControllerManufacturer, Controller, SupportWorkflow
+from .forms import ControllerManufacturerForm, ControllerForm, SupportWorkflowForm
 
-# Create your views here.
+def add_controller_manufacturer(request):
+    if request.method == 'POST':
+        form = ControllerManufacturerForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('add_controller_manufacturer')
+    else:
+        form = ControllerManufacturerForm()
+    return render(request, 'add_controller_manufacturer.html', {'form': form})
+
+def add_controller(request):
+    if request.method == 'POST':
+        form = ControllerForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('add_controller')
+    else:
+        form = ControllerForm()
+    return render(request, 'add_controller.html', {'form': form})
+
+def add_support_workflow(request):
+    if request.method == 'POST':
+        form = SupportWorkflowForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('add_support_workflow')
+    else:
+        form = SupportWorkflowForm()
+    return render(request, 'add_support_workflow.html', {'form': form})
+
+def view_database(request):
+    manufacturers = ControllerManufacturer.objects.all()
+    controllers = Controller.objects.all()
+    workflows = SupportWorkflow.objects.all()
+
+    context = {
+        'manufacturers': manufacturers,
+        'controllers': controllers,
+        'workflows': workflows,
+    }
+
+    return render(request, 'view_database.html', context)
+
+def troubleshooting(request):
+    manufacturers = ControllerManufacturer.objects.all()
+    selected_manufacturer = None
+    selected_controller = None
+    workflows = None
+
+    if 'manufacturer' in request.GET:
+        selected_manufacturer = ControllerManufacturer.objects.get(id=request.GET['manufacturer'])
+        controllers = selected_manufacturer.controllers.all()
+    else:
+        controllers = None
+
+    if 'controller' in request.GET and controllers:
+        selected_controller = Controller.objects.get(id=request.GET['controller'])
+        workflows = selected_controller.support_workflows.all()
+
+    context = {
+        'manufacturers': manufacturers,
+        'controllers': controllers,
+        'workflows': workflows,
+        'selected_manufacturer': selected_manufacturer,
+        'selected_controller': selected_controller,
+    }
+    return render(request, 'troubleshooting.html', context)
